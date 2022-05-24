ad9b0b5f7f205231edcc85ec9fad92ddadb4b23c
chenpq05 <chenpq05@vanke.com>
2022-05-24 13:38:37 +0800

    GeneratorCode.java

--- examples/example-parent/boot-web/src/test/java/com/github/codingsoldier/bootweb/GeneratorCode.java
+++ examples/example-parent/boot-web/src/test/java/com/github/codingsoldier/bootweb/GeneratorCode.java
@@ -2,19 +2,27 @@ package com.github.codingsoldier.bootweb;
 
 
 import com.github.codingsoldier.starter.mybatisplus.generator.MybatisPlusCodeGenerator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class GeneratorCode {
+    private static final Logger LOGGER = LoggerFactory.getLogger(GeneratorCode.class);
 
     /**
      * 注意，旧文件不会被覆盖
      */
     public static void main(String[] args) {
+
+        LOGGER.info("打印信息吗");
+
+
         // 数据库url
-        MybatisPlusCodeGenerator.dbUrl = "";
+        // MybatisPlusCodeGenerator.dbUrl = "jdbc:mysql://172.16.84.29:3306/2_3_aface_material?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&useSSL=false";
+        MybatisPlusCodeGenerator.dbUrl = "jdbc:mysql://172.16.84.29:3306/easygo_edge?useUnicode=true&characterEncoding=UTF-8&zeroDateTimeBehavior=convertToNull&useSSL=false";
         // 数据库账号
-        MybatisPlusCodeGenerator.dbUsername = "";
+        MybatisPlusCodeGenerator.dbUsername = "easygo";
         // 数据库密码
-        MybatisPlusCodeGenerator.dbPassword = "";
+        MybatisPlusCodeGenerator.dbPassword = "Vr@Dev123";
 
         // 项目 main 目录的绝对路径
         MybatisPlusCodeGenerator.srcMainAbsolutePath = "D:\\mycode\\micro-service\\examples\\example-parent\\boot-web\\src\\main";
