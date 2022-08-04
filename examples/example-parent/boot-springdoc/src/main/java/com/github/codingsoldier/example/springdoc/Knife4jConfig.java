03d701138d9985d4706a6c25219abfa49286a4cb
CodingSoldier <tfz9011@163.com>
2022-08-04 23:51:41 +0800

    测试Knife4j

---
+++ examples/example-parent/boot-springdoc/src/main/java/com/github/codingsoldier/example/springdoc/Knife4jConfig.java
@@ -0,0 +1,27 @@
+// package com.github.codingsoldier.example.springdoc;
+//
+// import org.springframework.context.annotation.Bean;
+// import org.springframework.context.annotation.Configuration;
+//
+// @Configuration
+// public class Knife4jConfig {
+//     @Bean(value = "defaultApi2")
+//     public Docket defaultApi2() {
+//         Docket docket=new Docket(DocumentationType.SWAGGER_2)
+//                 .apiInfo(new ApiInfoBuilder()
+//                         //.title("swagger-bootstrap-ui-demo RESTful APIs")
+//                         .description("# swagger-bootstrap-ui-demo RESTful APIs")
+//                         .termsOfServiceUrl("http://www.xx.com/")
+//                         .contact("xx@qq.com")
+//                         .version("1.0")
+//                         .build())
+//                 //分组名称
+//                 .groupName("2.X版本")
+//                 .select()
+//                 //这里指定Controller扫描包路径
+//                 .apis(RequestHandlerSelectors.basePackage("com.github.xiaoymin.knife4j.controller"))
+//                 .paths(PathSelectors.any())
+//                 .build();
+//         return docket;
+//     }
+// }
