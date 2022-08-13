4f46924673cda46787b7f4192be8b547f4291671
CodingSoldier <tfz9011@163.com>
2022-08-13 13:53:20 +0800

    sentinel

--- examples/example-parent/cloud-web-01/src/main/java/com/github/codingsoldier/example/cloudweb01/feign/timeout/Web02FeignTimeoutClient.java
+++ examples/example-parent/cloud-web-01/src/main/java/com/github/codingsoldier/example/cloudweb01/feign/timeout/Web02FeignTimeoutClient.java
@@ -6,20 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
- * fallbackFactory：可以获取 feign 调用异常，服务端未启动，会执行 fallbackFactory 的降级逻辑
  * fallback：无法获取 feign 调用异常，服务端未启动，不会执行 fallback 的降级逻辑
+ * fallbackFactory：可以获取 feign 调用异常，服务端未启动，会执行 fallbackFactory 的降级逻辑
+ * 若同时存在 fallback、fallbackFactory，优先使用 fallback
  *
- * @FeignClient 不能同时使用 fallbackFactory、fallback 两种属性
- * <p>
  * spring-cloud 2020 弃用 Hystrix
  * 可使用 sentinel 替代 hystrix
  */
-// @FeignClient(value = "cloud-web-02", contextId= "hystrixClient", path = "/cloud-web-02/hystrix"
-// , fallbackFactory = HystrixClientFallbackFactory.class)
 
-// @FeignClient(value = "cloud-web-02", contextId= "web02Feign02Client", path = "/cloud-web-02/feign02/test", fallback= TimeoutFallback.class)
-
-@FeignClient(value = "cloud-web-02", contextId = "web02FeignTimeoutClient", path = "/cloud-web-02/feign02/test", fallbackFactory = TimeoutFallbackFactory.class)
+// @FeignClient(value = "cloud-web-02", contextId = "web02FeignTimeoutClient", path = "/cloud-web-02/feign02/test",
+//         fallback = TimeoutFallback.class, fallbackFactory = TimeoutFallbackFactory.class)
+@FeignClient(value = "cloud-web-02", contextId = "web02FeignTimeoutClient", path = "/cloud-web-02/feign02/test",
+        fallbackFactory = TimeoutFallbackFactory.class)
 public interface Web02FeignTimeoutClient {
 
     @GetMapping(value = "/timeout01", produces = MediaType.APPLICATION_JSON_VALUE)
