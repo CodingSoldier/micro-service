5b3690583a6b7004f67118d17a5e2cb57d07e73a
CodingSoldier <tfz9011@163.com>
2022-09-03 14:56:24 +0800

    部署

---
+++ examples/example-parent/boot-web/src/main/java/com/github/codingsoldier/example/bootweb/StartCommandRunner.java
@@ -0,0 +1,20 @@
+package com.github.codingsoldier.example.bootweb;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class StartCommandRunner implements CommandLineRunner {
+
+    @Value("${profile-val:未配置}")
+    private String profileVal;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("###################profile-val={}", profileVal);
+    }
+
+}
