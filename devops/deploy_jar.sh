12ccf1c554691b277948e045e4c3e3f21c210987
CodingSoldier <tfz9011@163.com>
2022-09-04 03:37:55 +0800

    部署

--- devops/deploy_jar.sh
+++ devops/deploy_jar.sh
@@ -2,7 +2,6 @@
 
 jar_name=$1
 sleep_time=$2
-start_command=$3
 
 echo "【信息】jar包名称 $jar_name"
 
@@ -18,13 +17,14 @@ fi
 
 sleep $sleep_time
 
+project_pid=$(ps -ef | grep $jar_name.jar | grep java | awk '{print $2}')
 if [ -n "$project_pid" ]; then
     sudo kill -9 $project_pid
 fi
 
 # 备份
-mkdir backup
+mkdir -p backup
 sudo cp -f $jar_name.jar backup/$jar_name.jar.`date '+%Y%m%d_%H.%M.%S'`
 
 # 启动
-$start_command
+{{jarStartCommand}}
