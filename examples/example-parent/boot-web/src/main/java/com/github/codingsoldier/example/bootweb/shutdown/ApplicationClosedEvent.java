4117d67603eb1461c88dac89756f2efad49c4fde
CodingSoldier <tfz9011@163.com>
2022-09-04 01:20:22 +0800

    部署

--- examples/example-parent/boot-web/src/main/java/com/github/codingsoldier/example/bootweb/shutdown/ApplicationClosedEvent.java
+++ examples/example-parent/boot-web/src/main/java/com/github/codingsoldier/example/bootweb/shutdown/ApplicationClosedEvent.java
@@ -3,8 +3,10 @@ package com.github.codingsoldier.example.bootweb.shutdown;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.event.ContextClosedEvent;
+import org.springframework.stereotype.Component;
 
 @Slf4j
+@Component
 public class ApplicationClosedEvent implements ApplicationListener<ContextClosedEvent> {
     @Override
     public void onApplicationEvent(ContextClosedEvent event) {
