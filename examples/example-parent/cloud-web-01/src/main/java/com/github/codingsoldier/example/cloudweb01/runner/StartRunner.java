b7880ecde5de56f31dbc841b9f720518e6055204
CodingSoldier <tfz9011@163.com>
2022-09-04 18:17:53 +0800

    部署

---
+++ examples/example-parent/cloud-web-01/src/main/java/com/github/codingsoldier/example/cloudweb01/runner/StartRunner.java
@@ -0,0 +1,21 @@
+package com.github.codingsoldier.example.cloudweb01.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class StartRunner implements CommandLineRunner {
+
+    @Value("${test-nacos:null}")
+    private String testNacos;
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("###################test-nacos={}", testNacos);
+    }
+
+}
