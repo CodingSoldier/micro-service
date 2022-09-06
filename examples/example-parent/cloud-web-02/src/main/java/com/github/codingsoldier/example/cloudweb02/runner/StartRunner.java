b6751c710be7e9dc316e335bd727e30904cb2c16
CodingSoldier <tfz9011@163.com>
2022-09-07 00:03:27 +0800

    部署

---
+++ examples/example-parent/cloud-web-02/src/main/java/com/github/codingsoldier/example/cloudweb02/runner/StartRunner.java
@@ -0,0 +1,21 @@
+package com.github.codingsoldier.example.cloudweb02.runner;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class StartRunner implements CommandLineRunner {
+
+    @Value("${profile-val:未配置}")
+    private String profileVal;
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("###################profile-val={}", profileVal);
+        log.info("@@@@@@@@@@@@@@@@@@@@@@@@@修改了代码");
+    }
+
+}
