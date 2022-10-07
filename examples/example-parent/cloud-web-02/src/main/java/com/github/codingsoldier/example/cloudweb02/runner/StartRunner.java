e89714355d2b3f25f2773da89e414f0af625aec0
CodingSoldier <tfz9011@163.com>
2022-10-07 20:12:30 +0800

    修改ip

--- examples/example-parent/cloud-web-02/src/main/java/com/github/codingsoldier/example/cloudweb02/runner/StartRunner.java
+++ examples/example-parent/cloud-web-02/src/main/java/com/github/codingsoldier/example/cloudweb02/runner/StartRunner.java
@@ -15,7 +15,7 @@ public class StartRunner implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         log.info("###################profile-val={}", profileVal);
-        log.info("@@6666666修改了代码");
+        log.info("@@777777777777修改了代码");
     }
 
 }
