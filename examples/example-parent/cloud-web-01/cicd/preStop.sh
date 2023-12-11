c917bc0535793d3316b559167ffc67a7f45b6745
陈丕迁 <chenpq05@vanke.com>
2023-12-11 18:00:13 +0800

    刷新缓存

--- examples/example-parent/cloud-web-01/cicd/preStop.sh
+++ examples/example-parent/cloud-web-01/cicd/preStop.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 # nacos客户端实例下线
 curl --location --request GET 'http://127.0.0.1:8001/cloud-web-01/nacos/graceful/deregister/instance'
-# 睡眠一段时间，推荐大于或等于20，等待gateway网关将微服务剔除，不再接收流量
-sleep 20
+# 睡眠一段时间，等待gateway网关将微服务剔除，不再接收流量
+sleep 10
