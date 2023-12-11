bde96b8101c93c8dc6a7c1a4143d6c2095ea3920
CodingSoldier <tfz9011@163.com>
2023-12-11 23:37:09 +0800

    cicd

--- examples/example-parent/cloud-web-01/src/main/java/com/github/codingsoldier/example/cloudweb01/nacos/TestNacosController.java
+++ examples/example-parent/cloud-web-01/src/main/java/com/github/codingsoldier/example/cloudweb01/nacos/TestNacosController.java
@@ -38,7 +38,7 @@ public class TestNacosController {
     public Result<String> graceful() throws Exception {
         int i = new Random().nextInt(1000, 5000);
         TimeUnit.MILLISECONDS.sleep(i);
-        return Result.success("Vbbbbbbbbb-"+i);
+        return Result.success("Vccccccccccc-"+i);
     }
 
     @PostMapping(value = "/graceful2")
