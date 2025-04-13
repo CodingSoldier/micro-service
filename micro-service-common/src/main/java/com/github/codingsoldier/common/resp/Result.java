f720767f080ce1a70751593d4c689759ac534afd
CodingSoldier <tfz9011@163.com>
2025-04-13 17:02:16 +0800

    Result.java code

--- micro-service-common/src/main/java/com/github/codingsoldier/common/resp/Result.java
+++ micro-service-common/src/main/java/com/github/codingsoldier/common/resp/Result.java
@@ -17,10 +17,19 @@ public class Result<T> implements Serializable {
   private static final long serialVersionUID = 1L;
 
   @Schema(description = """
-        1、code为具体的业务编码，code = 20000 表示成功。<br/>
-        2、40000 <= code <= 49999，客户端错误，例如：参数填写错误。<br/>
-        3、50000 <= code <= 59999 , 服务端错误，例如：空指针异常。
-        """, example = "20000")
+     <br /> 规范：
+     <br /> 1、code统一为5位。
+     <br /> 2、20000 <= code <= 29999 表示成功，其他表示失败。
+     <br /> 3、客户端错误，以4开头。例如：
+     <br />   http status = 429 表示“并发请求过多”，则使用 42900 - 42999 表示“并发请求过多” 这类的异常，
+     <br />   例如：使用 42901 表示“系统忙，您发送的请求被限流了”。
+     <br /> 4、服务端错误，以5开头。
+     <br />   http status = 500 表示“服务器端程序错误”，则使用 50000 - 50099 表示“并服务器端程序错误” 这类的异常。
+     <br /> 5、后端约定http status = 200只表明服务端能就收到请求并返回结果，并不意味着服务端能正确处理请求。
+     <br />   当http status = 200，code的取值范围是大于等于20000，小于等于59999，
+     <br />   所以当http status = 200时，也要判断code的值。
+     <br /> 6、建议：前端收到响应，40000 <= code <= 49999，使用黄色弹出框；50000 <= code <= 59999，使用红色弹出框。
+     """, example = "20000")
   private int code;
 
   @Schema(description = "提示信息")
